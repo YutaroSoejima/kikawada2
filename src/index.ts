456890a237cb979e0336f3246e623157d123b595
SKikawada <sc1995065@gmail.com>
2023-11-04 20:23:28 +0900

    Update index.ts
    port 8080

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import type { Express, Request, Response } from 'express';
 
 const app: Express = express();
-const port = 3001;
+const port = 8080;
 
 app.get('/', (req: Request, res: Response) => res.send('Hello World!'));
 
